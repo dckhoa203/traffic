05875d5f5dc8983cdb046c7f081a76ff4535143c
Khoa <khoadochi1998@gmail.com>
2020-12-07 14:58:10 +0700

    add script refresh database

---
+++ freshdb.sh
@@ -0,0 +1,6 @@
+php artisan migrate:fresh
+php artisan db:seed
+php artisan db:seed --class=ProvincesSeeder
+php artisan db:seed --class=DistrictsSeeder
+php artisan db:seed --class=VehicleTypesSeeder
+php artisan db:seed --class=TollStationsSeeder
